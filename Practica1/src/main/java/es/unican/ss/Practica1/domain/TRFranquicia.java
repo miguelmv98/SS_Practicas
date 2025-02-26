db27a723df93b093823cc5fe18f6c580d17552e6
Miguel Monje Velarde <mmv776@alumnos.unican.es>
2025-02-26 11:02:15 +0100

    Practica  1 fin

--- Practica1/src/main/java/es/unican/ss/Practica1/domain/TRFranquicia.java
+++ Practica1/src/main/java/es/unican/ss/Practica1/domain/TRFranquicia.java
@@ -1,12 +1,19 @@
 package es.unican.ss.Practica1.domain;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlType;
 import java.io.Serializable;
 import java.time.LocalDate;
 
 @SuppressWarnings({ "serial" })
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name="TRFranquicia")
 public class TRFranquicia extends Seguro implements Serializable {
 	
 	private static final int PRECIO_BASE_TR_FRANQUICIA = 400;
+	@XmlAttribute(required=true)
 	private double franquicia;
 	
 	public TRFranquicia() {
